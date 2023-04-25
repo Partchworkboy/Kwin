5a6396f92445f95431a25db236bc5901b4503010
Fred <59137832+Partchworkboy@users.noreply.github.com>
2023-04-25 23:52:53 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,7 +10,6 @@ st.set_page_config(page_title="Kwin Stationers - The sky is a podium",
                    page_icon=":pencil2:",
                    layout="wide")
 
-if selected_menu == "Home":
   st.title("Welcome to Kwin Stationers!")
   st.markdown("""
             We are a small business that provides high-quality products and services to our customers. Our goal is to make your life easier and more enjoyable by providing you with the tools and resources you need to succeed.
