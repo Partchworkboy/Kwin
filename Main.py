a4d9a6993d1a316039adb384955c3a7361d44ad7
Fred <59137832+Partchworkboy@users.noreply.github.com>
2023-04-25 23:59:49 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,15 +10,9 @@ st.set_page_config(page_title="Kwin Stationers - The sky is a podium",
                    page_icon=":pencil2:",
                    layout="wide")
 
-  st.title("Welcome to Kwin Stationers!")
-  st.markdown("""
-            We are a small business that provides high-quality products and services to our customers. Our goal is to make your life easier and more enjoyable by providing you with the tools and resources you need to succeed.
-        """)
-
-elif selected_menu == "Dashboard":
-  st.title("Dashboard")
-  st.markdown("""
-            This is the dashboard.
+st.title("Welcome to Kwin Stationers!")
+st.markdown("""
+          We are a small business that provides high-quality products and services to our customers. Our goal is to make your life easier and more enjoyable by providing you with the tools and resources you need to succeed.
         """)
 
 # Add page footer
